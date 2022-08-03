7e5ebdc0dcb9879fe72e76e9846684d5ecd5cdb3
nurynurish <potterharry200200.com>
2022-08-03 20:16:15 +0200

    did small changes

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ function taxCalculator(){
     price = document.getElementById("price").value;
     tax = (price * tax_rate) /100;
     total = parseInt(price) + parseInt(tax);
-    resultText = "Starting price of the product:" +"<strong>" + price +"</strong>" + "<br>"
-                +"The tax amount of the product:" +"<strong>" + tax +"</strong>" + "<br>"
-                + "Selling price of the product:" +"<strong>" + total + "</strong>";
+    resultText = "Starting price of the product: " +"<strong>" + price +"</strong>" + "<br>"
+                +"The tax amount of the product: " +"<strong>" + tax +"</strong>" + "<br>"
+                + "Selling price of the product: " +"<strong>" + total + "</strong>";
     document.getElementById("result").innerHTML = resultText;
 }
