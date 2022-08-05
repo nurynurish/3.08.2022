ed04c6c307cbf2efc07c651510b6b4fee030af77
nurynurish <potterharry200200.com>
2022-08-05 16:10:54 +0200

    Added EventListener

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function taxCalculator(){
     price = document.getElementById("price").value;
     tax = (price * tax_rate) /100;
     total = parseInt(price) + parseInt(tax);
+    ////////////////////////////////////////
     resultText = "Starting price of the product: " +"<strong>" + price +"</strong>" + "<br>"
                 +"The tax amount of the product: " +"<strong>" + tax +"</strong>" + "<br>"
                 + "Selling price of the product: " +"<strong>" + total + "</strong>";
